d63ee0f2c7f9ea7ff813905969fb839127a67293
Errelin <leon.errelin@outlook.com>
2020-11-04 17:05:16 +0800

    lisp cmt convention + drop use of org-bullets

--- .config/emacs.d/init.el
+++ .config/emacs.d/init.el
@@ -1,21 +1,21 @@
-;; Use on singel config file
-;; To stop pre-optimization
-;; Once the config grows over 500 lines
-;; Start modularizing it then
+;;;; Use on singel config file
+;;;; To stop pre-optimization
+;;;; Once the config grows over 500 lines
+;;;; Start modularizing it then
 
 ;;(add-to-list 'load-path
 ;;	     (expand-file-name (concat user-emacs-directory "lisp")))
 
 
 
-;; (require 'init-const)
-;; ------------------- init-const  ---------------------
+;;; (require 'init-const)
+;;; ------------------- init-const  ---------------------
 (defconst *is-mac* (eq system-type 'darwin))
 (defconst *is-linux* (eq system-type 'gnu/linux))
 (defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
 
-;; ------------------- init-startup  ---------------------
-;; set the default encoding system
+;;; ------------------ change default ---------------------
+;;; set the default encoding system
 (prefer-coding-system 'utf-8)
 (setq default-file-name-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -24,16 +24,15 @@
 (setq default-buffer-file-coding-system 'utf-8)
 
 
-;; ------------------ change default ---------------------
 ;; (auto-save-mode -1)
 (setq make-backup-files nil)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
+(setq inhibit-compacting-font-caches t)
 
 
-
-;;------------------- about company ----------------------
+;;; ------------------- about company ----------------------
 (global-company-mode t)
 (define-key company-active-map (kbd "<tab>") 'company-select-next)
 (define-key company-active-map (kbd "p") 'company-select-previous)
@@ -41,7 +40,7 @@
 
 (setq inhibit-startup-screen t)
 
-;; -------------------  init-pkgs  ---------------------
+;;; -------------------  init-pkgs  ---------------------
 (use-package benchmark-init
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
@@ -54,7 +53,7 @@
 
 
 ;;(require 'init-elpa)
-;; -------------------  init-elpa ---------------------
+;;; -------------------  init-elpa ---------------------
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
 			 ("org"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
@@ -89,14 +88,14 @@
 ;; restart emacs
 (require 'use-package)
 
-;; -------------------  init-theme ---------------------
+;;; -------------------  init-theme ---------------------
 ;; (require 'init-ui)
 ;; gruvbox theme
-;;(use-package gruvbox-theme
-;; :init (load-theme 'gruvbox-dark-soft t))
+(use-package gruvbox-theme
+ :init (load-theme 'gruvbox-dark-soft t))
 
-(use-package spacemacs-theme
-  :init (load-theme 'spacemacs-dark t))
+;;(use-package spacemacs-theme
+;;  :init (load-theme 'spacemacs-dark t))
 
 ;; status line
 (use-package smart-mode-line
@@ -111,7 +110,7 @@
 
 
 ;; (require 'init-pkgs)
-;; -------------------  init-ui ---------------------
+;;; -------------------  init-ui ---------------------
 (use-package benchmark-init
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
@@ -123,31 +122,57 @@
 (use-package restart-emacs)
 
 ;;(require 'org-mode)			     
-;; ------------------- org-mode ---------------------
+;;; ------------------- org-mode ---------------------
 ;; org-mode keys
-(setq org-agenda-include-diary t)
+
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(setq org-agenda-include-diary t)
+(setq org-agenda-files '("~/proj/org-roam/"))
+
+;; regarding *org-startup-folded*
+;; ="content"= won't work! but ='content= works!
+;; try it out on Emacs 27.1
+;; see https://brantou.github.io/2017/03/21/just-try/
+(setq
+ org-deadline-warning-days 0
+ org-startup-folded 'content
+ org-hide-leading-stars t)
 
 ;; org-mode to do
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)")
-	(sequence "CANCELLED(c)" "|" "EVENT(e)")
-	(sequence "⋇TODO(i)" "|" "»(f)" "«(o)"))) 
+	(sequence "CANCELLED(c)" "|" "EVENT(e)" "IDEA(a)" "WATCH(w)")
+	(sequence "✝TODO(i)" "|" ">(f)" "<(o)")))
+
 ;; right arrow: migrate to Futher; left arrow: migrate to Other collections
 
-;; ------------------ org bullets -------------------
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(setq org-bullets-bullet-list '("◎" "◉" "○" "●"))
+;;; ------------------ org bullets -------------------
+;;; enabling bullet will *significantly* slow the performance, see
+;;; 1. https://github.com/sabof/org-bullets/issues/11
+;;; 2. https://www.reddit.com/r/emacs/comments/98flwy/does_anyone_know_a_good_alternative_to_orgbullets/
+;;; 3. https://www.reddit.com/r/emacs/comments/8ug077/orgbullets_slow_with_certain_fonts/
+;;; (require 'org-bullets)
+;;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+;;; (setq org-bullets-bullet-list '("⌬" "◉" "●" "○"))
 
-;; ------------------ org journal -------------------
-(setq org-journal-dir "~/proj/org-bujo/")
-(setq org-journal-date-format "%m/%d %a")
+;;; ------------------ org journal -------------------
+;;; customized so to adjust to Bullet Journal style
 
-(require 'org-journal)
+(use-package org-journal
+  :custom
+  (org-journal-dir "~/proj/org-roam/")
+  (org-journal-file-type 'daily)
+  (org-journal-date-format "%m.%d %a")
+  ;; must contain %Y
+  ;; see https://github.com/bastibe/org-journal/blob/78d575213c7cf7c069e52bfc46430090c0500000/org-journal.el#L158
+  (org-journal-file-format "%Y-%m-%d.org")
+  (org-journal-time-prefix "** %R")
+  :config
+  (setq org-journal-find-file #'find-file-other-window
+	org-journal-enable-agenda-integration t))
 
-;; write cutome stuff to a singel file
+;;; write cutome stuff to a singel file
 (setq custom-file "~/.emacs.d/custom-file.el")
 (load-file custom-file)
