6e0fc291346366c782134887ee286856165a6bd5
Errelin <leon.errelin@outlook.com>
2020-11-14 23:43:25 +0800

    custom file added

---
+++ .config/emacs.d/custom-file.el
@@ -0,0 +1,22 @@
+
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (foreground "#076678")))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/leon/org/bujo/20-nov.org" "/home/leon/org/bujo/20-oct.org" "/home/leon/org/bujo/FUTURE-LOG.org" "/home/leon/org/bujo/index.org"))
+ '(package-selected-packages
+   '(org-roam-server which-key use-package smart-mode-line restart-emacs org-roam org-journal-list org-journal org-bullets gruvbox-theme deft company benchmark-init)))
