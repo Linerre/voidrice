2803e8ced4a1a195dc021888e563485f35a2a60f
errelin <leon.errelin@outlook.com>
2021-01-11 23:31:12 +0800

    fix a typo

--- .config/emacs.d/init.el
+++ .config/emacs.d/init.el
@@ -330,7 +330,7 @@
 ;;; 1. https://github.com/sabof/org-bullets/issues/11
 ;;; 2. https://www.reddit.com/r/emacs/comments/98flwy/does_anyone_know_a_good_alternative_to_orgbullets/
 ;;; 3. https://www.reddit.com/r/emacs/comments/8ug077/orgbullets_slow_with_certain_fonts/
-;;(require 'org-bullets)
+(require 'org-bullets)
 (add-hook 'org-mode-hook
 	  (lambda ()
 	    (org-bullets-mode t)))
