792a423cd8bf3b6ed53dfd4cb92174c100da5e68
Linerre <leon.errelin@outlook.com>
2021-03-27 23:35:28 +0800

    mod: make it for real .config dir

--- sync.sh
+++ sync.sh
@@ -12,14 +12,14 @@ new_config=0
 # loop over all configs under .config
 for dir in .config/*; do
 	program="$(basename $dir)"	
-	if [ -d "$HOME/config/$program" ]; then
+	if [ -d "$HOME/.config/$program" ]; then
 		printf "%s %s\n" "$program" "config directory already exists; Overriding..."
-		cp -f -r "$dir"/* "$HOME/config/$program/"
+		cp -f -r "$dir"/* "$HOME/.config/$program/"
 		existing_config=$((existing_config+1))
 	else
 		printf "%s %s\n" "Need to copy over config for" "$program"
-		mkdir "$HOME/config/$program"
-		cp -r "$dir"/* "$HOME/config/$program/"
+		mkdir "$HOME/.config/$program"
+		cp -r "$dir"/* "$HOME/.config/$program/"
 		new_config=$((new_config+1))
 	fi
 done
