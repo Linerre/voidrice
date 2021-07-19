8c47ea8e22bf6c171ea2dd61a56dea2e9c31995b
Linerre <leon.errelin@outlook.com>
2021-07-20 00:55:30 +0800

    just start company for now

--- .config/emacs/lisp/init-company.el
+++ .config/emacs/lisp/init-company.el
@@ -2,13 +2,8 @@
 ;;; ------------------- COMPANY  -----------------------
 ;;; ----------------------------------------------------
 (use-package company
-  :init
-  (global-company-mode t)
-  (define-key company-active-map (kbd "<tab>") 'company-select-next)
-;;(define-key company-active-map (kbd "<esc>") 'company-select-previous)
   :config
-  (setq company-idle-delay 0.0
-      inhibit-startup-screen t))
+  (setq global-company-mode t))
 
 (provide 'init-company)
 ;;; init-company ends here
